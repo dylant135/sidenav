de11987de3a32c08a5d7f037cfad65210215e3e0
dylan <dylangthomas1@gmail.com>
2022-12-03 12:49:55 -0700

    layout setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div className="App">
       <h1>Side Nav</h1>
-      <Header />
-      <Sidebar />
+      <div className='navContainer'>
+        <Sidebar />
+        <div className='rightContainer'>
+          <Header />
+          <MainContent />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="sideInfo"></div>
-            <div className="sideInfo"></div>
-            <div className="sideInfo"></div>
+            <div className="sideInfo">hi</div>
+            <div className="sideInfo">bye</div>
+            <div className="sideInfo">why</div>
         </div>
     )
 }
