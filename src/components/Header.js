6d37dad7823243e083f1d2563aa60a2f915cc94d
dylan <dylangthomas1@gmail.com>
2022-12-02 20:39:30 -0700

    project set up

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="headLink">link1</div>
+            <div className="headLink">link2</div>
+            <div className="headLink">link3</div>
+        </div>
+    )
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sideInfo"></div>
+            <div className="sideInfo"></div>
+            <div className="sideInfo"></div>
+        </div>
+    )
+}
